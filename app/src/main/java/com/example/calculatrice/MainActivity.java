9a38bfb9e32796edf205d9123ae655cc02dee12a
carion21 <medimickael99@gmail.com>
2019-11-30 19:50:01 +0000

    peaufinage : ajout d'une icone pour l'application , suppression de la title bar et changement du theme par defaut

--- app/src/main/java/com/example/calculatrice/MainActivity.java
+++ app/src/main/java/com/example/calculatrice/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.calculatrice.databinding.ActivityMainBinding;
 
@@ -61,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         decimalFormat = new DecimalFormat("#.##########");
 
